d7082cfbb5bc6fc8edb6cad434b8d785346815ec
Shake <robin.smekens.930@gmail.com>
2020-10-11 00:45:38 +0200

    added back player movement component

--- Sable/src/Game/Components/PlayerMovementComponent.h
+++ Sable/src/Game/Components/PlayerMovementComponent.h
@@ -1,12 +1,28 @@
 ﻿#pragma once
 
-namespace Shake
+#include "Shake/Serialization/metastuff/example/nlohmann_json/json.hpp""
+#include "Shake/Serialization/metastuff/include/Meta.h"
+
+struct PlayerMovementComponent
 {
-    struct PlayerMovementComponent
+    std::string Name = "PlayerMovementComponent";
+    float MovementSpeedIncrease{50.0f}; // 0.5f
+    float MaxMovementSpeed{10.0f}; // 10.0f
+    float VelocityMultiplier{50.0f}; // 0.9f
+    float JumpVelocity{5.0f};
+};
+
+namespace meta
+{
+    template <>
+    inline auto registerMembers<PlayerMovementComponent>()
     {
-        float MovementSpeedIncrease{50.0f}; // 0.5f
-        float MaxMovementSpeed{10.0f}; // 10.0f
-        float VelocityMultiplier {50.0f}; // 0.9f
-        float JumpVelocity{5.0f};
-    };
+        return members(
+            member("name", &PlayerMovementComponent::Name),
+            member("movementSpeedIncrease", &PlayerMovementComponent::MovementSpeedIncrease),
+            member("maxMovementSpeed", &PlayerMovementComponent::MaxMovementSpeed),
+            member("velocityMultiplier", &PlayerMovementComponent::VelocityMultiplier),
+            member("jumpVelocity", &PlayerMovementComponent::JumpVelocity)
+        );
+    }
 }
--- Shake_Engine/src/Shake/Scene/Scene.cpp
+++ Shake_Engine/src/Shake/Scene/Scene.cpp
@@ -126,149 +126,6 @@ namespace Shake
         SELOG(LogVerbosity::Info, "{0}", entities.size());
     }
 
-    void SceneX::TestMetaStuff()
-    {
-        Person person;
-        person.age = 25;
-        person.salary = 3.50f;
-        person.name = "Alex"; // I'm a person!
-
-        person.favouriteMovies["Nostalgia Critic"] = {MovieInfo{"The Room", 8.5f}};
-        person.favouriteMovies["John Tron"] = {
-            MovieInfo{"Goosebumps", 10.0f},
-            MovieInfo{"Talking Cat", 9.0f}
-        };
-
-        // printing members of different classes
-        std::cout << "Members of class Person:\n";
-        meta::doForAllMembers<Person>(
-            [](const auto& member)
-            {
-                std::cout << "* " << member.getName() << '\n';
-            }
-        );
-
-        std::cout << "Members of class MovieInfo:\n";
-        meta::doForAllMembers<MovieInfo>(
-            [](const auto& member)
-            {
-                std::cout << "* " << member.getName() << '\n';
-            }
-        );
-
-        printSeparator();
-
-        // checking if classes are registered
-        if (meta::isRegistered<Person>())
-        {
-            std::cout << "Person class is registered\n";
-            std::cout << "It has " << meta::getMemberCount<Person>() << " members registered.\n";
-        }
-
-        // meta::isRegistered is constexpr, so can be used in enable_if and static_assert!
-        static_assert(meta::isRegistered<Person>(), "Person class is not registered!");
-        static_assert(meta::getMemberCount<Person>() == 4, "Person does not have 4 members registered?");
-
-        if (!meta::isRegistered<Unregistered>())
-        {
-            std::cout << "Unregistered class is unregistered\n";
-            std::cout << "It has " << meta::getMemberCount<Unregistered>() << " members registered.\n";
-        }
-
-        // checking if class has a member
-        if (meta::hasMember<Person>("age"))
-        {
-            std::cout << "Person has member named 'age'\n";
-        }
-
-        // getting members
-        auto age = meta::getMemberValue<int>(person, "age");
-        std::cout << "Got person's age: " << age << '\n';
-
-        auto name = meta::getMemberValue<std::string>(person, "name");
-        std::cout << "Got person's name: " << name << '\n';
-
-        // setting members
-        meta::setMemberValue<std::string>(person, "name", "Ron Burgundy");
-        name = meta::getMemberValue<std::string>(person, "name");
-        std::cout << "Changed person's name to " << name << '\n';
-
-        printSeparator();
-
-        // And here's how you can serialize/deserialize
-        // (if you write a function for your type)
-        std::cout << "Serializing person:" << '\n';
-        json root = person;
-        ///std::cout << std::setw(4) << root << std::endl;
-
-        //Unregistered y;
-        //json root2 = y; // this will fail at compile time
-
-        printSeparator();
-
-        std::cout << "Serializing Person 2 from JSON:\n";
-
-        auto person2 = root.get<Person>();
-        std::cout << "Person 2 name is " << person2.getName() << " too!" << '\n';
-    }
-
-    void SceneX::TestOwnMetaStuff()
-    {
-        TransformComponent transformComponent{SVector3(0.0f)};
-
-        // printing members of different classes
-        std::cout << "Members of class Person:\n";
-        meta::doForAllMembers<TransformComponent>(
-            [](const auto& member)
-            {
-                std::cout << "* " << member.getName() << '\n';
-            }
-        );
-
-        printSeparator();
-
-        // checking if classes are registered
-        if (meta::isRegistered<TransformComponent>())
-        {
-            std::cout << "TransformComponent class is registered\n";
-            std::cout << "It has " << meta::getMemberCount<TransformComponent>() << " members registered.\n";
-        }
-
-        // checking if class has a member
-        if (meta::hasMember<TransformComponent>("rotation"))
-        {
-            std::cout << "TransformComponent has member named 'rotation'\n";
-        }
-
-        // getting members
-        auto rotation = meta::getMemberValue<float>(transformComponent, "rotation");
-        std::cout << "Got transform rotation: " << rotation << '\n';
-
-        // setting members
-        meta::setMemberValue<float>(transformComponent, "rotation", 1.0f);
-        rotation = meta::getMemberValue<float>(transformComponent, "rotation");
-        std::cout << "Changed rotation to" << rotation << '\n';
-
-        printSeparator();
-
-        // And here's how you can serialize/deserialize
-        // (if you write a function for your type)
-        std::cout << "Serializing transformComponent:" << '\n';
-        json root = transformComponent;
-        //std::cout << std::setw(4) << root << std::endl;
-
-
-        auto testTransformComponent = root.get<TransformComponent>();
-        rotation = meta::getMemberValue<float>(testTransformComponent, "rotation");
-        std::cout << "Changed rotation to" << rotation << '\n';
-
-        // MetaTestComponent component;
-        // component.test = 5.0f;
-        // json root(component);
-        // std::cout << std::setw(4) << root << std::endl;
-    }
-
-
     void SceneX::SaveScene()
     {
         std::string sceneData = "";
@@ -290,6 +147,10 @@ namespace Shake
                 if (entity.has_component<CollisionComponent>())
                 {
                     jsonEntity.push_back(*entity.component<CollisionComponent>().get());
+                }
+                if (entity.has_component<PlayerMovementComponent>())
+                {
+                    jsonEntity.push_back(*entity.component<PlayerMovementComponent>().get());
                 }
 
                 jsonEntities.push_back(jsonEntity);
@@ -364,6 +225,12 @@ namespace Shake
                     CollisionComponent newCollisionComponent;
                     from_json(temp, newCollisionComponent);
                     newEntity.assign<CollisionComponent>(newCollisionComponent);
+                }
+                if(otherTemp.value() == "PlayerMovementComponent")
+                {
+                    PlayerMovementComponent newPlayerMovementComponent;
+                    from_json(temp, newPlayerMovementComponent);
+                    newEntity.assign<PlayerMovementComponent>(newPlayerMovementComponent);
                 }
             }
         }
